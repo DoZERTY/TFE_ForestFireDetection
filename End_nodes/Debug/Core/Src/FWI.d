a785ae98ee02ddb15c352cfd11418b8639c37e2a
DoZERTY <dodo.schrob@gmail.com>
2024-11-07 22:09:54 +0100

    init

---
+++ End_nodes/Debug/Core/Src/FWI.d
@@ -0,0 +1,2 @@
+Core/Src/FWI.o: ../Core/Src/FWI.c ../Core/Inc/FWI.h
+../Core/Inc/FWI.h:
---
+++ End_nodes/Debug/Drivers/BME680/Src/bme68x.d
@@ -0,0 +1,5 @@
+Drivers/BME680/Src/bme68x.o: ../Drivers/BME680/Src/bme68x.c \
+ C:/Users/Home/Desktop/Q9/Thesis/Git_MS_thesis/TFE_ForestFireDetection/End_nodes/Drivers/BME680/Inc/bme68x.h \
+ C:/Users/Home/Desktop/Q9/Thesis/Git_MS_thesis/TFE_ForestFireDetection/End_nodes/Drivers/BME680/Inc/bme68x_defs.h
+C:/Users/Home/Desktop/Q9/Thesis/Git_MS_thesis/TFE_ForestFireDetection/End_nodes/Drivers/BME680/Inc/bme68x.h:
+C:/Users/Home/Desktop/Q9/Thesis/Git_MS_thesis/TFE_ForestFireDetection/End_nodes/Drivers/BME680/Inc/bme68x_defs.h:
